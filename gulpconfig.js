8707b004b53f35d8244e154420f636b698dfab6c
Jonas Fredriksson <jonas_fredriksson@hotmail.com>
2014-11-04 21:35:00 +0100

    Add sass task

---
+++ gulpconfig.js
@@ -0,0 +1,9 @@
+var dest = "./build";
+var src = './src';
+
+module.exports = { 
+  sass: {
+    src: src + "/static/style/*.scss",
+    dest: dest + "/static/style/"
+  }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
-var gulp = require('gulp');
+var config = require('./gulpconfig')
 
-gulp.task('default', function() {
-  // TODO: create awesome
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    gulp.src(config.sass.src)
+        .pipe(sass())
+        .pipe(gulp.dest(config.sass.dest));
+});
+
+gulp.task('build', ['sass']);
+
+gulp.task('default', ['build']);
+
+gulp.task('watch', ['build'], function(){
+	gulp.watch(config.sass.src, ['sass']);
 });
