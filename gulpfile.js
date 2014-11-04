3ad8a2085137d501e31dd81d5fe78bca18306b41
Jonas Fredriksson <jonas_fredriksson@hotmail.com>
2014-11-04 22:31:48 +0100

    Add css minify

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var config = require('./gulpconfig')
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minifycss = require('gulp-minify-css');
 
 gulp.task('sass', function () {
     gulp.src(config.sass.src)
         .pipe(sass())
+        .pipe(minifycss())
         .pipe(gulp.dest(config.sass.dest));
 });
 
